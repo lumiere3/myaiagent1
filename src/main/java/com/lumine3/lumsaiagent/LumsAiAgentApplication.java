b217e8dda4eacbe754a72718fefe1a0a0523a039
Lumine <1948906985@qq.com>
2025-10-25 17:47:14 +0800

    初始化

---
+++ src/main/java/com/lumine3/lumsaiagent/LumsAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.lumine3.lumsaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LumsAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LumsAiAgentApplication.class, args);
+    }
+
+}
