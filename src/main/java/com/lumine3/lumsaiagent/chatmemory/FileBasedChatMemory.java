5a6a5f5ad4a8d80afcededf506c293a5767c5d8a
Lumine <1948906985@qq.com>
2025-10-27 21:27:21 +0800

    项目开发1 2025-10-27

---
+++ src/main/java/com/lumine3/lumsaiagent/chatmemory/FileBasedChatMemory.java
@@ -0,0 +1,96 @@
+package com.lumine3.lumsaiagent.chatmemory;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+import org.objenesis.strategy.StdInstantiatorStrategy;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.messages.Message;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileBasedChatMemory implements ChatMemory {
+
+    //
+    private final String BASE_DIR;
+
+    private static final Kryo kryo = new Kryo();
+
+    //实例化策略
+    static {
+        kryo.setRegistrationRequired(false);
+        // 设置实例化策略
+        kryo.setInstantiatorStrategy(new StdInstantiatorStrategy());
+    }
+    // 构造对象时，指定文件保存目录
+    public FileBasedChatMemory(String dir) {
+        this.BASE_DIR = dir;
+        File baseDir = new File(dir);
+        if (!baseDir.exists()) {
+            baseDir.mkdirs();
+        }
+    }
+
+
+    @Override
+    public void add(String conversationId, List<Message> messages) {
+
+    }
+
+    @Override
+    public List<Message> get(String conversationId, int lastN) {
+        return List.of();
+    }
+
+    @Override
+    public void clear(String conversationId) {
+
+    }
+
+
+    /**
+     * 获取或创建会话信息的列表
+     * @param conversationId
+     */
+    private List<Message> getOrCreateConversation(String conversationId) {
+        File file = getConversationFile(conversationId);
+        List<Message> messages = new ArrayList<>();
+        if (file.exists()) {
+            try (Input input = new Input(new FileInputStream(file))) {
+                messages = kryo.readObject(input, ArrayList.class);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return messages;
+    }
+
+    /**
+     * 保存对话信息
+     * @param conversationId
+     * @param messages
+     */
+    private void saveConversation(String conversationId, List<Message> messages) {
+        File file = getConversationFile(conversationId);
+        try (Output output = new Output(new FileOutputStream(file))) {
+            kryo.writeObject(output, messages);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * 每个会话按照id单独保存会话
+     * @param conversationId
+     * @return
+     */
+    private File getConversationFile(String conversationId) {
+        return new File(BASE_DIR, conversationId + ".kryo");
+    }
+}
